79f5a1dc76c02f114478c94026886c0f827fe4f3
felipeFleite <felipefracalossileite@gmail.com>
2024-08-09 14:57:32 -0400

    home

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from "./Layout"
+import Home from "./Home"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Layout/>}/>
+      <Route path="/" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,

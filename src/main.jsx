2afd78d2df58e704d356166d3515c25711586dfb
felipeFleite <felipefracalossileite@gmail.com>
2024-08-09 14:41:25 -0400

    rooter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from "./Layout"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 

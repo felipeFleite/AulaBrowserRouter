1a5637244abd9f21b8675519205b9ed717c1d22d
felipeFleite <felipefracalossileite@gmail.com>
2024-08-12 16:52:15 -0400

    carousel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,9 +1,19 @@
 import Header from "../components/Header"
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 export default function Home() {
     return(
         <>
-        <Header/>
-        <h1>Hello Kit</h1>
+        <Carousel
+        <div>
+        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRicA5AYQwSbfKZZlB52Ar_yLUfdu_mRzTMRA&s" alt="Carro"></img>
+        </div>
+        <div>
+        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dMRIWoNixPmIw5wfxWm90vHEdE72rPJqkA&s" alt="Foto de homem na frente de um carro"
+        </div>
+        >
+        </Carousel>
         </>
     )
 }

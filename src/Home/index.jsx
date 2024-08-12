09a67a44355a9f27040e6e66be29031b305e2254
felipeFleite <felipefracalossileite@gmail.com>
2024-08-12 16:57:35 -0400

    carouselconfig

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -5,14 +5,23 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 export default function Home() {
     return(
         <>
-        <Carousel
+        <Carousel>
+        infiniteLoop
+        autoPlay
+        usekeyboardArrows
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}
+        dynamicHeight
         <div>
         <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRicA5AYQwSbfKZZlB52Ar_yLUfdu_mRzTMRA&s" alt="Carro"></img>
         </div>
         <div>
-        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dMRIWoNixPmIw5wfxWm90vHEdE72rPJqkA&s" alt="Foto de homem na frente de um carro"
+        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dMRIWoNixPmIw5wfxWm90vHEdE72rPJqkA&s" alt="Foto de homem na frente de um carro"></img>
         </div>
-        >
+        <div>
+        <img src ="https://www.designi.com.br/images/preview/10172138.jpg" alt="Carro Branco"></img>
+        </div>
         </Carousel>
         </>
     )

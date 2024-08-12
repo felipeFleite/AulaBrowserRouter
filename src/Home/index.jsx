5aa44ab8179309a0f2933ac2e1ad73df493ae06e
felipeFleite <felipefracalossileite@gmail.com>
2024-08-12 17:08:21 -0400

    carouselcerto

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,10 @@
-import Header from "../components/Header"
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Home() {
     return(
         <>
-        <Carousel>
+        <Carousel
         infiniteLoop
         autoPlay
         usekeyboardArrows
@@ -13,15 +12,20 @@ export default function Home() {
         showStatus={false}
         showThumbs={false}
         dynamicHeight
+        >
+            
         <div>
-        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRicA5AYQwSbfKZZlB52Ar_yLUfdu_mRzTMRA&s" alt="Carro"></img>
+        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRicA5AYQwSbfKZZlB52Ar_yLUfdu_mRzTMRA&s" alt="Carro"/>
         </div>
+
         <div>
-        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dMRIWoNixPmIw5wfxWm90vHEdE72rPJqkA&s" alt="Foto de homem na frente de um carro"></img>
+        <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dMRIWoNixPmIw5wfxWm90vHEdE72rPJqkA&s" alt="Foto de homem na frente de um carro"/>
         </div>
+
         <div>
-        <img src ="https://www.designi.com.br/images/preview/10172138.jpg" alt="Carro Branco"></img>
+        <img src ="https://www.designi.com.br/images/preview/10172138.jpg" alt="Carro Branco"/>
         </div>
+
         </Carousel>
         </>
     )

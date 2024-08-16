8d2c61980aea54d9d5c41fa8f101278ff15beece
felipeFleite <felipefracalossileite@gmail.com>
2024-08-16 17:04:49 -0400

    a

--- src/components/ListarProdutos.jsx
+++ src/components/ListarProdutos.jsx
@@ -4,30 +4,7 @@ export default function ListaProduto() {
           {id: 1, nome: "a", preco: "R$ 2,00"},
           {id: 2, nome: "b", preco: "R$ 5,00"},
           {id: 3, nome: "c", preco: "R$ 12,00"},
-        ])
-      
-        const [listaPedidos, setListaPedidos] = useState([])
-      
-        const adicionarItemPedidos = (objeto) => {
-            setListaPedidos([...listaPedidos,objeto])
-        }
-      
-        const removerPedido = (id) => {
-          let remover = false
-          let listaAux = listaPedidos.filter((produto)=> {
-            if(remover == false) {
-              if(produto.id != id) {
-                return produto
-              } else{
-                remover = true
-                return null
-              }
-            } else {
-              return produto
-            }
-          })
-          setListaPedidos(listaAux)
-        }
+        ])   
         return (
           <div>
               
@@ -36,20 +13,11 @@ export default function ListaProduto() {
                 <div key = {produto.id}>
                 <p>{produto.nome}</p>
                 <p>{produto.preco}</p>
-                <button onClick={()=> adicionarItemPedidos(produto)}>Comprar</button>
-                </div>
-                )
-              }
-              {
-                listaPedidos.map((produto)=>
-                <div key = {produto.id}>
-                <p>{produto.nome}</p>
-                <p>{produto.preco}</p>
-                <button onClick={()=> removerPedido(produto.id)}>Remover</button>
+                <button>Comprar</button>
                 </div>
                 )
               }
           </div>
          
         );
-      }
+            }

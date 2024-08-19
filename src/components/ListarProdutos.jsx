7fb25761d6bb98f9502808c0f5490cc9d6dfd940
felipeFleite <felipefracalossileite@gmail.com>
2024-08-18 23:12:26 -0400

    a

--- src/components/ListarProdutos.jsx
+++ src/components/ListarProdutos.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import "/src/globals.css"
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
 export default function ListaProduto() {
         const [listaProduto, setProduto] = useState([
-          {id: 1, nome: "a", preco: "R$ 2,00"},
-          {id: 2, nome: "b", preco: "R$ 5,00"},
-          {id: 3, nome: "c", preco: "R$ 12,00"},
-          {id: 4, nome: "c", preco: "R$ 12,00"},
-          {id: 5, nome: "c", preco: "R$ 12,00"},
-          {id: 6, nome: "c", preco: "R$ 12,00"},
-          {id: 7, nome: "c", preco: "R$ 12,00"},
-          {id: 8, nome: "c", preco: "R$ 12,00"},
-          {id: 9, nome: "c", preco: "R$ 12,00"},
-          {id: 10, nome: "c", preco: "R$ 12,00"},
-          {id: 11, nome: "c", preco: "R$ 12,00"},
-          {id: 12, nome: "c", preco: "R$ 12,00"},
-          {id: 13, nome: "c", preco: "R$ 12,00"},
-          {id: 14, nome: "c", preco: "R$ 12,00"},
-          {id: 15, nome: "c", preco: "R$ 12,00"},
+          {id: 1, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 2, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 3, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 4, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 5, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 6, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 7, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 8, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 9, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 10, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 11, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 12, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 13, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 14, nome: "a", preco: "R$ 2,00", imgUrl: ""},
+          {id: 15, nome: "a", preco: "R$ 2,00", imgUrl: ""},
         ])   
+        const [listaPedidos, setlistaPedidos] = useState([])
+        const adicionarPedido = (nome) => {
+          toast(`O produto foi adicionado ao carrinho`)
+          setlistaPedidos([...listaPedidos, nome])
+      }
         return (
           <div id="cards">
               
@@ -25,13 +32,14 @@ export default function ListaProduto() {
                 
                 listaProduto.map((produto) =>
                 <div id ="produte" key = {produto.id}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0YQ0U77Xk4LJu3u1-E-0YIxlLRJcneKhOAA1jWlnbcr4i2qP9l_5tYibzA&s"/>
+                <img src={produto.imgUrl}/>
                 <p class="text">{produto.nome}</p>
                 <p class="text">{produto.preco}</p>
-                <button>Comprar</button>
+                <button onClick={adicionarPedido}>Comprar</button>
                 </div>
                 )
               }
+               <ToastContainer/>
           </div>
          
         );

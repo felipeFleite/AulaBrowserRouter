813068083d501b90b6be9ca8f30cfe89e456d375
felipeFleite <felipefracalossileite@gmail.com>
2024-08-19 23:04:36 -0400

    a

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -20,11 +20,11 @@ export default function Home() {
         >
 
         <div>
-        <img src ="https://www.designi.com.br/images/preview/10243023.jpg" alt="Carro"/>
+        <img src ="https://cdn.appdealersites.com.br/barigui/eva/conteudo/388.webp" alt="Carro"/>
         </div>
 
         <div>
-        <img src ="https://www.designi.com.br/images/preview/11876061.jpg" alt="Foto de homem na frente de um carro"/>
+        <img src ="https://cdn.appdealersites.com.br/discasa/agosto/PC%20-%20T-Cross%20(1).webp" alt="Foto de homem na frente de um carro"/>
         </div>
 
         <div>

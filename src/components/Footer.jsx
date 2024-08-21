c0c21e5a5f6e42a6e0de76e20433426e003d8b9d
felipeFleite <felipefracalossileite@gmail.com>
2024-08-21 18:52:14 -0400

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import "/src/globals.css"
-export default function Header() {
+export default function Footer() {
     return(
         <>
         <footer id="footer">
             <p>Telefone para Contato: (19)8119-9733</p>
-            <img src="https://static.vecteezy.com/ti/vetor-gratis/t2/3541644-icon-social-media-design-set-gratis-vetor.jpg" alt="" />
+            <img id="fotimg"src="img\image.png"/>
         </footer>
         </>
     )

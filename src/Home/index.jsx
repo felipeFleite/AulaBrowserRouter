0a0b184af87586a0a55ee2d8e79c64ea64bd91f5
felipeFleite <felipefracalossileite@gmail.com>
2024-08-21 09:27:09 -0400

    fin

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -3,11 +3,20 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import Header from "../components/Header"
 import "/src/globals.css"
 import Footer from "../components/Footer"
-import ListarPoucosProdutos from '../components/ListarPoucosProdutos'
-
+import ListarProdutos from '../components/ListarProdutos'
+import { useState } from 'react'
 export default function Home() {
+    const [listaProduto, setProduto] = useState([
+        {id: 1, promocao: 25, marca: "Fiat", nome: "Titano", preco: "R$ 219.000,00", imgUrl: "https://www.fiat.com.br/content/dam/fiat/nova-home/carrossel/titano.webp"},
+        {id: 2, promocao: 0, marca: "Fiat", nome: "500e", preco: "R$ 214.000,00", imgUrl: "https://www.fiat.com.br/content/dam/fiat/nova-home/carrossel/500e.webp"},
+        {id: 3, promocao: 25, marca: "Fiat", nome: "Mobi", preco: "R$ 73.000,00", imgUrl: "https://www.fiat.com.br/content/dam/fiat/nova-home/carrossel/mobi.webp"},
+        {id: 4, promocao: 25, marca: "Fiat", nome: "Argo", preco: "R$ 85.000,00", imgUrl: "https://www.fiat.com.br/content/dam/fiat/nova-home/carrossel/argo.webp"},
+        {id: 5, promocao: 0, marca: "Fiat", nome: "Cronos", preco: "R$ 94.000,00", imgUrl: "https://www.fiat.com.br/content/dam/fiat/nova-home/carrossel/cronos.webp"},
+        ]) 
+
     return(
         <>
+        
         <Header/>
         <Carousel
         infiniteLoop
@@ -33,7 +42,7 @@ export default function Home() {
 
         </Carousel>
         <h1 id="title">PRODUTOS</h1>
-        <ListarPoucosProdutos/>
+        <ListarProdutos lista={listaProduto}/>
         <Footer/>
         </>
     )
